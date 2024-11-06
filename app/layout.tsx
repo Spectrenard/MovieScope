501cc084b502cd31e9f341f0bddc520d6439fee5
spectre <e.sahinnn61@gmail.com>
2024-11-06 01:20:00 +0100

    add: scrolltotop

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import "./globals.css";
 
 export default function RootLayout({
@@ -11,12 +12,9 @@ export default function RootLayout({
     <html lang="fr">
       <body>
         <Navbar />
-        <div className="pt-16">
-          {" "}
-          {/* Espace pour la navbar fixe */}
-          {children}
-        </div>
+        <div className="pt-16">{children}</div>
         <Footer />
+        <ScrollToTop />
       </body>
     </html>
   );
